1d6a9ad14464b9b53d624fac0b1dea2cbd3eee64
rei___yu <satomi.i.sakura@gmail.com>
2015-10-12 14:54:53 +0900

    change the Theme and the reaction to Menu bar

--- app/src/main/java/com/example/satomi/simplefeatures/MyActivity.java
+++ app/src/main/java/com/example/satomi/simplefeatures/MyActivity.java
@@ -1,14 +1,16 @@
 package com.example.satomi.simplefeatures;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.content.Intent;
 import android.view.View;
 import android.widget.EditText;
 
-public class MyActivity extends ActionBarActivity {
+public class MyActivity extends Activity {
 
     public final static String EXTRA_MESSAGE = "com.satomi.myfirstapp.MESSAGE";
     @Override
@@ -19,9 +21,9 @@ public class MyActivity extends ActionBarActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_my, menu);
-        return true;
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_my, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
@@ -29,14 +31,12 @@ public class MyActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
         }
-
-        return super.onOptionsItemSelected(item);
     }
 
     public void sendMessage(View View) {
